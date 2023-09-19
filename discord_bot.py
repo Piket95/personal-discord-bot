6779a861d57db8b1141c4f14a9376319acdb85f6
Dennis Adam <dennis.adam95@gmail.com>
2023-09-19 06:27:42 +0200

    generalized code and outsourced specific configurations or data

--- discord_bot.py
+++ discord_bot.py
@@ -1,5 +1,3 @@
-import json
-import os
 from datetime import datetime
 
 import discord
@@ -11,16 +9,11 @@ import youtube_api
 
 class DiscordBot(discord.Client):
 
-    genshinNewsChannel: discord.channel.TextChannel
-
     async def setup_hook(self) -> None:
-        self.check_genshin_yt_channel.start()
+        self.check_for_new_yt_videos.start()
 
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
-
-        # get genshin news channel object
-        self.genshinNewsChannel = self.get_channel(int(os.getenv('GENSHIN_IMPACT_NEWS_CHANNEL_ID')))
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -29,17 +22,24 @@ class DiscordBot(discord.Client):
         print(f'Message from {message.author}: {message.content}')
 
     @tasks.loop(hours=6)
-    async def check_genshin_yt_channel(self):
+    async def check_for_new_yt_videos(self):
         now = datetime.now(pytz.timezone('Europe/Berlin'))
 
-        print(f'[{now.strftime("%d.%m.%y, %H:%M:%S")}] Checking genshin yt channel')
+        print(f'[{now.strftime("%d.%m.%y, %H:%M:%S")}] Checking for new YouTube Videos')
 
-        message = youtube_api.check_genshin_yt_channel()
+        messageObj = youtube_api.check_for_new_yt_videos()
 
-        if message:
-            await self.genshinNewsChannel.send(message)
+        rolesToPing = '@everyone'
 
-    @check_genshin_yt_channel.before_loop
+        if messageObj:
+            for message in messageObj:
+                text = (f'||{ rolesToPing }||\nNeue Sondersendung vom **{ message["videoDate"] } '
+                           f'Uhr** aus der Playlist "{ message["playlistName"] }"\n{ message["url"] }')
+
+                destDCChannel = self.get_channel(int(message['destDCChannelId']))
+                await destDCChannel.send(text)
+
+    @check_for_new_yt_videos.before_loop
     async def before_yt_check(self):
 
         await self.wait_until_ready()
--- youtube_api.py
+++ youtube_api.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 import pytz
 import requests
@@ -10,51 +11,94 @@ YOUTUBE_BASE_VIDEO_URL = 'https://www.youtube.com/watch?v='
 MAX_PLAYLIST_RESULTS = 1  # need only one. if the video id isn't the saved one the queried one is newer
 
 
-def check_genshin_yt_channel():
-    GENSHIN_IMPACT_PLAYLIST_ID = os.getenv('GENSHIN_IMPACT_SONDERSENDUNG_PL_ID')
+def load_playlist_config() -> dict:
 
-    savedJsonData = None
+    playlistConfig = {}
+
+    try:
+        with open('config/yt-video-feeds.json', 'r') as f:
+            playlistConfig = json.load(f)
+    except FileNotFoundError:
+        print('\033[91mFehler: Die Config-Datei unter "config/yt-video-feeds.json" fehlt!\033[0m')
+        sys.exit()
+
+    return playlistConfig
+
+
+def check_for_new_yt_videos():
+
+    playlistConfig = None
+    savedJsonData = {}
+
+    # load playlist config
+    playlistConfig = load_playlist_config()
 
     # get saved yt video id from saved json file or set predefined layout if file doesn't exist
     try:
         with open('data/saved_yt_video_id.json', 'r') as f:
             savedJsonData = json.load(f)
     except Exception as error:
-        print('An exception has occurred: ', error)
+        if not isinstance(error, FileNotFoundError):
+            print(f'\033[91mAn exception has occurred: {error}\033[0m')
 
-    if savedJsonData is None:
-        savedJsonData = {
-            'lastSavedVideoId': ''
-        }
+    queriedVideos = []
 
-    parameter = {
-        'part': 'snippet,contentDetails',
-        'playlistId': GENSHIN_IMPACT_PLAYLIST_ID,
-        'maxResults': MAX_PLAYLIST_RESULTS,
-        'key': os.getenv('YT_API_KEY')
-    }
+    if bool(playlistConfig) and 'playlistFeeds' in playlistConfig:
+        for playlistFeed in playlistConfig['playlistFeeds']:
+            if playlistFeed['skip']:
+                continue
 
-    headers = {
-        'Accept': 'application/json'
-    }
+            parameter = {
+                'part': 'snippet,contentDetails',
+                'playlistId': playlistFeed['playlistId'],
+                'maxResults': playlistFeed['maxPlaylistResults'],
+                'key': os.getenv('YT_API_KEY')
+            }
 
-    response = requests.get(YOUTUBE_API_BASE_URL + '/playlistItems', params=parameter, headers=headers)
-    data = response.json()
-    newestVideoId = data['items'][0]['contentDetails']['videoId']
-    videoDate = data['items'][0]['snippet']['publishedAt']
+            headers = {
+                'Accept': 'application/json'
+            }
 
-    message = False
+            response = requests.get(YOUTUBE_API_BASE_URL + '/playlistItems', params=parameter, headers=headers)
+            data = response.json()
+            newestVideoId = data['items'][0]['contentDetails']['videoId']
+            videoDate = data['items'][0]['snippet']['publishedAt']
 
-    if savedJsonData['lastSavedVideoId'] != newestVideoId:
-        savedJsonData['lastSavedVideoId'] = newestVideoId
+            queriedVideos.append({
+                'name': playlistFeed['name'].replace(" ", "-").lower(),
+                'playlistName': playlistFeed['name'].replace(" ", "-").lower(),
+                'videoId': newestVideoId,
+                'videoDate': videoDate,
+                'destDCChannelId': playlistFeed['destinationDCChannelId']
+            })
 
-        formattedVideoDate = (parser.parse(videoDate).astimezone(pytz.timezone('Europe/Berlin'))
-                              .strftime('%d.%m.%Y, %H:%M:%S'))
+    messageObj = []
 
-        urlToPost = YOUTUBE_BASE_VIDEO_URL + newestVideoId
-        message = f'Neue Sondersendung vom **{formattedVideoDate} Uhr**\n{urlToPost}'
+    if bool(queriedVideos):
+
+        for video in queriedVideos:
+
+            if not video['name'] in savedJsonData:
+                savedJsonData[video['name']] = {}
+
+            if ('lastSendVideoId' not in savedJsonData[video['name']]
+                    or savedJsonData[video['name']]['lastSendVideoId'] != video['videoId']):
+
+                savedJsonData[video['name']]['lastSendVideoId'] = video['videoId']
+
+                formattedVideoDate = (parser.parse(video['videoDate']).astimezone(pytz.timezone('Europe/Berlin'))
+                                      .strftime('%d.%m.%Y, %H:%M:%S'))
+
+                urlToPost = YOUTUBE_BASE_VIDEO_URL + video['videoId']
+
+                messageObj.append({
+                    'playlistName': video['playlistName'],
+                    'url': urlToPost,
+                    'destDCChannelId': video['destDCChannelId'],
+                    'videoDate': formattedVideoDate
+                })
 
     with open('data/saved_yt_video_id.json', 'w') as f:
         f.write(json.dumps(savedJsonData, indent=4))
 
-    return message
+    return messageObj
